0482d422d66050d2e15d4123f542052452920b6c
Guhansekar <162482317+Guhansekar@users.noreply.github.com>
2025-09-19 23:00:22 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,70 @@
+import "./App.css"
+import { useState, useEffect } from "react";
+
+const images = [
+  { img: "1.jpg" },
+  { img: "2.JPG" },
+   { img: "1.jpg" },
+  { img: "2.JPG" },
+  { img: "1.jpg" }
+];
+
+function App() {
+  const [startIdx, setStartIdx] = useState(0);
+  const total = images.length;
+
+  const handlePrev = () => {
+    setStartIdx((prev) => (prev === 0 ? total - 1 : prev - 1));
+  };
+
+  const handleNext = () => {
+    setStartIdx((prev) => (prev + 1) % total);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setStartIdx((prev) => (prev + 1) % total);
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, [total]);
+
+  const getDisplayImages = () => [
+    images[startIdx % total],
+    images[(startIdx + 1) % total],
+    images[(startIdx + 2) % total],
+     images[(startIdx + 3) % total],
+    images[(startIdx + 4) % total],
+    images[(startIdx + 5) % total]
+  ];
+
+  return (
+    <>
+      <div className="display">
+      <div style={{ display: "flex", alignItems: "center", gap: "16px"  }}>
+        <button onClick={handlePrev}>
+          &#8592;
+        </button>
+        <div style={{ display: "flex", gap: "20px" }}>
+          {getDisplayImages().map((img, idx) => (
+           <div className="imgslide"> <img
+              key={idx}
+              src={img.img}
+              alt={`Slide ${startIdx + idx + 1}`}
+              style={{ width: "300px", height: "300px", objectFit: "cover" }}
+            />
+            <p>comfort</p>
+            </div>
+          ))}
+          
+        </div>
+        <button onClick={handleNext} className="next">
+          &#8594;
+        </button>
+      </div>
+      </div>
+    </>
+  );
+}
+
+export default App
